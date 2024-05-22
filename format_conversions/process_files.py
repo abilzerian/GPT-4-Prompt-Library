5d1a80bb2d0bd4eb2414e67bc7c46edb5a2b5ec1
Pablo Vega-Behar <pablovega@live.com>
2024-05-22 03:19:02 -0400

    generalize functions

--- format_conversions/process_files.py
+++ format_conversions/process_files.py
@@ -2,14 +2,17 @@ import os
 from typing import Callable
 
 from format_conversions.converters import (
-    FileReaderFactory, FileWriter, process_markdown, process_json, process_plain_text
+    FileReaderFactory, FileWriter, process_markdown, process_json, process_plain_text, BaseMetadata
 )
 
 PARENT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
-def get_directories(path):
-    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+def get_directories(path, exclude_dirs=None):
+    dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    if not exclude_dirs:
+        exclude_dirs = []
+    return [d for d in dirs if not d.startswith('.') and d not in exclude_dirs]
 
 
 def make_dirs(path, directories):
@@ -17,14 +20,10 @@ def make_dirs(path, directories):
         os.makedirs(os.path.join(path, directory), exist_ok=True)
 
 
-def process_directories():
+def process_directories(output_dir='processed_prompts', exclude_dirs=None):
     parent_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    output_path = os.path.join(parent_path, 'processed_prompts')
-    dirs = get_directories(parent_path)
-
-    # Skip hidden directories and the processed_prompts dir
-    dirs = [d for d in dirs if not d.startswith('.') and d not in ['processed_prompts', 'venv']]
-
+    output_path = os.path.join(parent_path, output_dir)
+    dirs = get_directories(parent_path, exclude_dirs)
     print(dirs)
     make_dirs(output_path, dirs)
 
@@ -35,14 +34,25 @@ def process_files(
         output_path: str = os.path.join(PARENT_PATH, 'processed_prompts')
 ) -> None:
     file_reader = FileReaderFactory(input_path).create()
-    data = processing_function(file_reader.data)
+    data = {}
+    try:
 
-    output = file_reader.process(*data)
+        data["title"], content = processing_function(file_reader.data)
 
-    output_path = os.path.join(output_path, input_path.split('/')[-2], f"{output.title}.json")
+        # If the title is not found in the metadata, use the filename
+        if not data["title"] or data["title"] == "No Title":
+            data["title"] = input_path.split('/')[-1].split('.')[0]
 
-    file_writer = FileWriter(data=output, file_path=output_path)
-    file_writer.write()
+        output = file_reader.process(data, content)
+
+        output_filename = f"{output.title.replace(' ', '_')}.json"
+
+        output_path = os.path.join(output_path, input_path.split('/')[-2], f"{output_filename}")
+
+        file_writer = FileWriter(data=output, file_path=output_path)
+        file_writer.write()
+    except Exception as e:
+        print(f"Error processing {input_path}\n. Raised:\n{e}")
 
 
 def rename_files_in_subdirectories(root_directory, exclude_dirs=None):
@@ -80,13 +90,16 @@ if __name__ == '__main__':
     if RENAME_FILES:
         rename_files_in_subdirectories(root_dir, EXCLUDE_DIRS)
 
-    INPUT_PATH = os.path.join(PARENT_PATH, 'miscellaneous')
-    for file in os.listdir(INPUT_PATH):
-        if file.endswith('.md'):
-            process_files(os.path.join(INPUT_PATH, file), processing_function=process_markdown)
-        elif file.endswith('.json'):
-            process_files(os.path.join(INPUT_PATH, file), processing_function=process_json)
-        elif file.endswith('.txt'):
-            process_files(os.path.join(INPUT_PATH, file), processing_function=process_plain_text)
-        else:
-            print(f"Unsupported file format: {file}")
+    input_dirs = get_directories(root_dir, EXCLUDE_DIRS)
+
+    for dirs_ in input_dirs:
+        input_path_ = os.path.join(PARENT_PATH, dirs_)
+        for file in os.listdir(input_path_):
+            if file.endswith('.md'):
+                process_files(os.path.join(input_path_, file), processing_function=process_markdown)
+            elif file.endswith('.json'):
+                process_files(os.path.join(input_path_, file), processing_function=process_json)
+            elif file.endswith('.txt'):
+                process_files(os.path.join(input_path_, file), processing_function=process_plain_text)
+            else:
+                print(f"Unsupported file format: {file}")
